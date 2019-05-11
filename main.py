deaeabdff1307810e1e19321afc9d2615ec923b2
Dmitry Fedyuk <dfediuk@gmail.com>
2019-05-12 02:51:04 +0600

    no message

--- main.py
+++ main.py
@@ -15,7 +15,19 @@ class_names = [
 1) `train_images` is an N-dimensional array: https://docs.scipy.org/doc/numpy/reference/arrays.ndarray.html
 2) The `shape` property returns the current shape of an array:
 https://docs.scipy.org/doc/numpy/reference/generated/numpy.ndarray.shape.html
-3) The output: (60000, 28, 28)
+3) The output: «(60000, 28, 28)»
 It means that the `train_images` array contains 60000 images of 28x28 pixels each. 
 '''
 print(train_images.shape)
+''' 2019-05-12
+1) «Return the length (the number of items) of an object.
+The argument may be a sequence (such as a string, bytes, tuple, list, or range) 
+or a collection (such as a dictionary, set, or frozen set).»
+https://docs.python.org/3.7/library/functions.html#len
+2) The output: «60000»
+'''
+print(len(train_labels))
+# 2019-05-12 The output: «[9 0 0 ... 3 0 5]»
+print(train_labels)
+# 2019-05-12 The output: «(10000, 28, 28)»
+print(test_images.shape)
